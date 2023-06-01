fc47a7b9c742ef493b132ec9445c1d822cb67273
Kelvin Almeida <58817347+kelvinalmeida@users.noreply.github.com>
2023-06-01 18:09:06 -0300

    add scren_screen

---
+++ lib/answer_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton(this.answerText, this.onTap, {super.key});
+
+  final String answerText;
+  final void Function() onTap;
+
+  @override
+  Widget build(context) {
+    return Container(
+      margin: const EdgeInsets.all(3),
+      child: ElevatedButton(
+        onPressed: onTap,
+        style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+          backgroundColor: const Color.fromARGB(255, 79, 7, 121),
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(40),
+          ),
+        ),
+        child: Text(
+          answerText,
+          style: const TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,8 +12,35 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  final currentQuestion = questions[0];
+
   @override
   Widget build(context) {
-    return const Center(child: Text('hello'));
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Text(
+                'Question ...',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 26,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            ...currentQuestion.answers.map(
+              (answer) => AnswerButton(answer, () {}),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
